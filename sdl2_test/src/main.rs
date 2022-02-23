a0c2d8b9032d079f50ce4d51d077b20213c9c0df
Alexander Schüssler <alex@xalx.net>
2022-02-23 16:27:55 +0100

    Do some sin/cos math to rotate the line we draw :D

--- sdl2_test/src/main.rs
+++ sdl2_test/src/main.rs
@@ -9,6 +9,7 @@ use sdl2::keyboard::Keycode;
 // use sdl2::gfx::primitives::ToColor;
 // use std::time::Duration;
 use std::{thread, time};
+use libm::{sin, cos};
 
 pub fn main() {
     // Init the SDL stuff
@@ -29,15 +30,27 @@ pub fn main() {
     let ten_millis = time::Duration::from_millis(10);
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
+    let mut angle = 0.0;
+    let angle_step = 0.01;
+    const PI:f64 = 3.14;
     // Named loop to succesfully break it
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        // i = (i + 1) % 255;
+        // canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.set_draw_color(Color::RGB(0, 255, 255));
         canvas.clear();
 
+        // Calculate sinus position and cast to i32 for drawing the
+        // funky line
+        angle = (angle + angle_step) % (2.0*PI);
+        let pos_x = (200.0*libm::cos(angle)) as i32 + 300;
+        let pos_y = (200.0*libm::sin(angle)) as i32 + 300;
+        println!("pos_x: {} pos_y: {}", pos_x, pos_y);
         canvas.set_draw_color(Color::RGB(255, 0, 255));
-        canvas.draw_line(Point::new(10, 10),
-                         Point::new(100, 100));
+        let aa:i32 = 10;
+        let bb:i32 = 100;
+        canvas.draw_line(Point::new(pos_x, pos_y),
+                         Point::new(pos_x+300, pos_y+300));
 
         // Need to pass by the window events or the window wont be
         // displayed at all
